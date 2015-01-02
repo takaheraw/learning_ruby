c9b58556e01d388f0c8a5040f7f65b140d2eddb8
takaheraw <takaheraw@gmail.com>
2015-01-02 10:10:35 +0900

    Command

---
+++ design_pattern/command.rb
@@ -0,0 +1,79 @@
+class Command
+  attr_reader :description
+
+  def initialize(description)
+    @description = description
+  end
+
+  def execute
+  end
+end
+
+class CreateFile < Command
+  def initialize(path, contents)
+    super "Create file: #{path}"
+    @path = path
+    @contents = contents
+  end
+
+  def execute
+    f = File.open(@path, "w")
+    f.write(@contents)
+    f.close
+  end
+
+  def unexecute
+    File.delete(@path)
+  end
+end
+
+class DeleteFile < Command
+  def initialize(path)
+    super "Delete file: #{path}"
+    @path = path
+  end
+
+  def execute
+    if File.exists?(@path)
+      @contents = File.read(@path)
+    end
+    f = File.delete(@path)
+  end
+
+  def unexecute
+    if @contents
+      f = File.open(@path, "w")
+      f.write(@contents)
+      f.close
+    end
+  end
+end
+
+class CompositeCommand < Command
+  def initialize
+    @commands = []
+  end
+
+  def add_command(cmd)
+    @commands << cmd
+  end
+
+  def execute
+    @commands.each do |cmd|
+      cmd.execute
+    end
+  end
+
+  def description
+    description = ''
+    @commands.each do |cmd|
+      description += cmd.description + "\n"
+    end
+    description
+  end
+end
+
+cmds = CompositeCommand.new
+cmds.add_command(CreateFile.new('file.txt', 'Hello world!'))
+cmds.add_command(DeleteFile.new('file.txt'))
+cmds.execute
