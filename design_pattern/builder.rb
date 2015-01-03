aecde1ba1b286c56aa213bd074e1ed8eec8c36f6
takaheraw <takaheraw@gmail.com>
2015-01-04 08:11:30 +0900

    Builder

---
+++ design_pattern/builder.rb
@@ -0,0 +1,81 @@
+class Computer
+  attr_accessor :display
+  attr_accessor :motherboard
+  attr_reader :drives
+
+  def initialize(display=:crt, motherboard=MotherBoard.new, drives=[])
+    @motherboard = motherboard
+    @drives = drives
+    @display = display
+  end
+end
+
+class CPU
+end
+
+class BasicCPU < CPU
+end
+
+class TurboCPU < CPU
+end
+
+class MotherBoard
+  attr_accessor :cpu
+  attr_accessor :memory_size
+
+  def initialize(cpu=BasicCPU.new, memory_size=1000)
+    @cpu = cpu
+    @memory_size = memory_size
+  end
+end
+
+class Drive
+  attr_reader :type
+  attr_reader :size
+  attr_reader :writable
+
+  def initialize(type, size, writable)
+    @type = type
+    @size = size
+    @writable = writable
+  end
+end
+
+class ComputerBuilder
+  attr_reader :computer
+
+  def initialize
+    @computer = Computer.new
+  end
+
+  def turbo(has_turbo_cpu=true)
+    @computer.motherboard.cpu = TurboCPU.new
+  end
+
+  def display=(display)
+    @computer.display = display
+  end
+
+  def memory_size(size_in_mb)
+    @computer.motherboard.memory_size = size_in_mb
+  end
+
+  def add_cd(writer=false)
+    @computer.drives << Drive.new(:cd, 760, writer)
+  end
+
+  def add_dvd(writer=false)
+    @computer.drives << Drive.new(:dvd, 4000, writer)
+  end
+
+  def add_hard_disk(size_in_mb)
+    @computer.drives << Drive.new(:hard_disk, size_in_mb, true)
+  end
+end
+
+builder = ComputerBuilder.new
+builder.turbo
+builder.add_cd(true)
+builder.add_dvd
+builder.add_hard_disk(100000)
+computer = builder.computer
