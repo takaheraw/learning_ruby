c629274198f1959e210cb909b516adb685c633ab
takaheraw <takaheraw@gmail.com>
2017-01-09 11:29:49 +0900

    コンポジション

---
+++ object_oriented_design/8.rb
@@ -0,0 +1,73 @@
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(args={})
+    @size  = args[:size]
+    @parts = args[:parts]
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+require 'forwardable'
+class Parts
+  extend Forwardable
+  def_delegators :@parts, :size, :each
+  include Enumerable
+
+  def initialize(parts)
+    @parts = parts
+  end
+
+  def spares
+    select{|part| part.needs_spare}
+  end
+end
+
+require 'ostruct'
+module PartsFactory
+  def self.build(config, parts_class = Parts)
+    parts_class.new(
+      config.collect{|part_config| create_part(part_config)}
+    )
+  end
+
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name: part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true)
+    )
+  end
+end
+
+road_config = [
+  ['chain',      '10-speed'],
+  ['tire_size',  '23'],
+  ['tape_color', 'red']
+]
+road_bike = Bicycle.new(
+  size: 'L',
+  parts: PartsFactory.build(road_config)
+)
+puts road_bike.size
+puts road_bike.spares.size
+puts road_bike.parts.size
+puts road_bike.parts.spares
+
+mountain_config = [
+  ['chain', '10-speed'],
+  ['tire_size', '2.1'],
+  ['front_shock', 'Manitou', false],
+  ['rear_shock', 'Fox']
+]
+mountain_bike = Bicycle.new(
+  size: 'L',
+  parts: PartsFactory.build(mountain_config)
+)
+puts mountain_bike.size
+puts mountain_bike.spares.size
+puts mountain_bike.parts.size
+puts mountain_bike.parts.spares
